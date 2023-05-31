0ba77b6d9f07a4d687252454d13e5cf65e1fe64f
alessandra martera <ale.martera@gmail.com>
2023-05-31 16:11:53 +0200

    starter settings

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script >
+
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <header>
+        <div>
+            Logo
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss' as *;
+
+header {
+    height: 150px;
+    background-color: #fff;
+    padding: 20px 10px;
+    display: flex;
+    justify-content: space-around;
+}
+</style>
