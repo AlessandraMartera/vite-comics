bf01ef194a55b853314cddcfa52b881cbaf4d9b6
alessandra martera <ale.martera@gmail.com>
2023-05-31 18:02:33 +0200

    update wrapper footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,102 @@
 <script >
 
 export default {
-    name: "AppFooter"
+    name: "AppFooter",
+    data() {
+        return {
+            navLinks: ['DIGITALS COMICS', 'DC MERCHANDISE', 'SUBSCRIPTION', 'COMIC SHOP LOCATOR', 'DC POWER VISA']
+        }
+    }
 }
 </script>
 
 <template>
-    <h1>ciao</h1>
+    <footer>
+        <!-- nav -->
+        <nav>
+            <div class="wrapper">
+                <ul>
+                    <li v-for="navLink in navLinks">
+                        {{ navLink }}
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+        <!-- content uls links -->
+        <div id="footer_content">
+        </div>
+        <!-- button section -->
+        <div id="buttons_footer">
+            <div class="wrapper">
+                <button>
+                    SIGN-UP NOW!
+                </button>
+
+                <div class="incos_links">
+                    follow us: fb tw yt pin maps
+                </div>
+            </div>
+
+        </div>
+    </footer>
 </template>
 
 <style lang="scss">
 @use '../styles/general.scss' as *;
+@use '../styles/variables.scss' as *;
+
+footer {
+    height: 600px;
+    color: #fff;
+
+
+    #footer_content {
+        height: calc(100% - 250px);
+        background-image: url(../assets/img/footer-bg.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+
+        .wrapper {}
+    }
+
+    nav {
+
+        background-color: $primary;
+
+        .wrapper {
+            height: 150px;
+
+            ul {
+                height: 100%;
+                display: flex;
+                justify-content: space-around;
+
+
+                li {
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                }
+            }
+        }
+
+
+    }
+
+    #buttons_footer {
+
+        background-color: #212121;
+
+        .wrapper {
+            height: 100px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+
+
+    }
+}
 </style>
